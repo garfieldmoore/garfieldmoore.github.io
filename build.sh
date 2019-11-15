f1923cccc9df9d99c5ef89fc8cfee45437de0bce
garfieldmoore <garfieldmoore@gmail.com>
2019-11-15 12:05:41 +0000

    compressed js files

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
+uglifyjs  --compress --mangle --output ./js/jquery.fitvids.min.js -- ./js/jquery.fitvids.js
+uglifyjs  --compress --mangle --output ./js/script.min.js -- ./js/script.js
+#uglifyjs  --compress --mangle --output ./assets/PgnViewerJS-0.9.7/js/pgnvjs.min.js -- ./assets/PgnViewerJS-0.9.7/js/pgnvjs.js
+
 bundle exec jekyll build
